ba30de7375e819e457faf2040d6f6a11947dc760
Jamie Brandon <jamie@scattered-thoughts.net>
2011-03-28 10:55:15 +0100

    Untested buckets module

--- src/pq_maps.erl
+++ src/pq_maps.erl
@@ -3,7 +3,7 @@
 
 -module(pq_maps).
 
--export([empty/0, is_empty/1, size/1, push_one/2, pop_one/1, push/2, pop/2, peek/1, to_list/1, from_list/1, delete/2]).
+-export([empty/0, is_empty/1, size/1, push_one/2, pop_lo/1, pop_hi/1, push/2, pop_los/2, peek/1, to_list/1, from_list/1, get/2, delete/2]).
 
 empty() ->
     gb_trees:empty().
@@ -22,30 +22,41 @@ push_one(Key, Value, Q) ->
 	    gb_trees:insert(Key, Value, Q)
     end.
 
-pop_one(Q) ->
+pop_lo(Q) ->
     case is_empty(Q) of 
 	true -> false;
 	false ->
 	    gb_trees:take_smallest(Q)
     end.
 
+pop_hi(Q) ->
+    case is_empty(Q) of 
+	true -> false;
+	false ->
+	    gb_trees:take_largest(Q)
+    end.
+
 push(Items, Q) ->
     lists:foldl(fun ({Key, Value}, Q2) -> push_one({Key, Value}, Q2) end, Q, Items).
 
-pop(Q, 0) ->
+pop_los(Q, 0) ->
     {[], Q};
-pop(Q, K) when K > 0 ->
+pop_los(Q, K) when K > 0 ->
     case is_empty(Q) of
 	true -> 
 	    {[], Q};
 	false ->
-	    {Key, Value, Q2} = pop_one(Q),
-	    {Items, Q3} = pop(Q2, K-1),
+	    {Key, Value, Q2} = pop_lo(Q),
+	    {Items, Q3} = pop_los(Q2, K-1),
 	    {[{Key, Value} | Items], Q3}
     end.
 
 peek(Q) ->
     gb_trees:smallest(Q).
+
+% assumes Key is in Q, crashes otherwise 
+get(Key, Q) ->
+    gb_trees:get(Key, Q).
 
 delete(Key, Q) ->
     gb_trees:delete_any(Key, Q).
@@ -55,3 +66,6 @@ to_list(Q) ->
 
 from_list(List) ->
     push(List, empty()).
+
+values(Q) ->
+    gb_trees:values(Q).
