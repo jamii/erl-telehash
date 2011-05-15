8e8d041bef5db5f52107017aa7ae4e1b4586bf08
Jamie Brandon <jamie@scattered-thoughts.net>
2011-05-15 21:28:17 +0800

    Types for th_bucket

--- src/th_bucket.erl
+++ src/th_bucket.erl
@@ -1,9 +1,9 @@
 % implements the bucket part of kademlia k-buckets
 % important points:
-% * each bucket should contain at most ?K nodes
-% * we should only ever report node addresses which we have personally confirmed
-% * responsive nodes should never be removed from buckets
-% * nodes should never be removed from buckets unless a suitable replacement exists
+% * each bucket should contain at most ?K peers
+% * we should only ever report peer addresses which we have personally confirmed
+% * responsive peers should never be removed from buckets
+% * peers should never be removed from buckets unless a suitable replacement exists
 % these make the router resilient to flooding, poisoning and network failure
 
 -module(th_bucket).
@@ -16,111 +16,132 @@
 
 -define(K, ?REPLICATION).
 
--record(node, {
-	  address, % node #address{} record
-	  'end', % node end
-	  suffix, % the remaining bits of the nodes end left over from the bit_tree
-	  status, % one of [live, stale, cache]
-	  last_seen % for live/stale nodes, the time of the last received message. for cache nodes the time of the last .see reference to the node
+-type status() :: live | stale | cache.
+
+-record(peer, {
+	  address :: address(), % peers address
+	  'end' :: 'end'(), % peers end
+	  suffix :: bits(), % the remaining bits of the peers end left over from the bit_tree
+	  status :: status(), % one of [live, stale, cache]
+	  last_seen :: now() % for live/stale peers, the time of the last received message. for cache peers the time of the last .see reference to the peer
 	 }).
+-type peer() :: #peer{}.
 
 -record(bucket, {
-	  last_dialed, % time an end in this bucket was last dialed, or never
-	  nodes, % gb_tree mapping addresses to {Status, Last_seen}
-	  % remaining fields are pq's of nodes sorted by their last_seen field
-	  live, % nodes currently expected to be alive
-	  stale, % nodes which have not replied recently
-	  cache % potential nodes which we have not yet verified 
+	  last_dialed :: never | now(), % time an end in this bucket was last dialed, or never
+	  peers :: gb_tree(), % gb_tree mapping addresses to {Status, Last_seen}
+	  % remaining fields are pq's of peers sorted by their last_seen field
+	  live :: pq_maps:pq(), % peers currently expected to be alive
+	  stale :: pq_maps:pq(), % peers which have not replied recently
+	  cache :: pq_maps:pq() % potential peers which we have not yet verified 
 	 }). % invariant: pq_maps:size(live) + pq_maps:size(stale) <= ?K
+-type bucket() :: #bucket{}.
+-export_types([bucket/0]).
 
 % --- api ---
 
+-spec empty() -> bucket().
+empty() ->
+    #bucket{
+       last_dialed = never,
+       peers = gb_trees:empty(),
+       live = pq_maps:empty(),
+       stale = pq_maps:empty(),
+       cache = pq_maps:empty()
+      }.
+
 % this address has been verified as actually existing
+-spec touched(address(), bits(), now(), bucket(), boolean()) -> th_bit_tree:bucket_update(bucket()).
 touched(Address, Suffix, Time, Bucket, May_split) ->
     ?INFO([touching, {address, Address}, {bucket, Bucket}]),
-    case get_node(Address, Bucket) of
-	{ok, Node} ->
-	    case Node#node.status of
+    case get_peer(Address, Bucket) of
+	{ok, Peer} ->
+	    case Peer#peer.status of
 		live -> 
 		    % update last_seen time
-		    ok(update_node(Node#node{last_seen=Time}, Bucket));
+		    ok(update_peer(Peer#peer{last_seen=Time}, Bucket));
 		stale ->
 		    % update last_seen time and promote to live
-		    ok(update_node(Node#node{last_seen=Time, status=live}, Bucket));
+		    ok(update_peer(Peer#peer{last_seen=Time, status=live}, Bucket));
 		cache ->
-		    % potentially promote the node to live
-		    Bucket2 = del_node(Node, Bucket),
-		    new_node(Address, Suffix, Time, Bucket2, May_split)
+		    % potentially promote the peer to live
+		    Bucket2 = del_peer(Peer, Bucket),
+		    new_peer(Address, Suffix, Time, Bucket2, May_split)
 	    end;
 	none ->
-	    % potentially add the node to live
-	    new_node(Address, Suffix, Time, Bucket, May_split)
+	    % potentially add the peer to live
+	    new_peer(Address, Suffix, Time, Bucket, May_split)
     end.
 
-% this address has been reported to exist by another node
-seen(Address, Time, Suffix, Bucket) ->
+% this address has been reported to exist by another peer
+-spec seen(address(), bits(), now(), bucket()) -> th_bit_tree:bucket_update(bucket()).
+seen(Address, Suffix, Time, Bucket) ->
     ?INFO([seeing, {address, Address}, {bucket, Bucket}]),
-    case get_node(Address, Bucket) of
-	{ok, Node} ->
-	    case Node#node.status of
+    case get_peer(Address, Bucket) of
+	{ok, Peer} ->
+	    case Peer#peer.status of
 		cache ->
-		    % for cache nodes being in a .see is good enough
-		    ok(update_node(Node#node{last_seen=Time}, Bucket));
+		    % for cache peers being in a .see is good enough
+		    ok(update_peer(Peer#peer{last_seen=Time}, Bucket));
 		_ ->
-		    % for live/stale nodes we require direct contact so ignore this
+		    % for live/stale peers we require direct contact so ignore this
 		    ok(Bucket)
 	    end;
 	none ->
-	    % put node in cache, return a live node to ping
-	    Node = #node{
+	    % put peer in cache, return a live peer to ping
+	    Peer = #peer{
 	      address = Address,
 	      'end' = th_util:to_end(Address),
 	      suffix = Suffix,
 	      status = cache,
 	      last_seen = Time
 	     },
-	    Bucket2 = add_node(Node, Bucket),
+	    Bucket2 = add_peer(Peer, Bucket),
 	    case peek_live_old(Bucket) of
 		none -> ok(Bucket2);
-		{ok, Live_node} -> {ping, Live_node#node.address, ok(Bucket2)}
+		{ok, Live_peer} -> {ping, Live_peer#peer.address, ok(Bucket2)}
 	    end
     end.
 
 % this address failed to reply in a timely manner
+-spec timedout(address(), bucket()) -> th_bit_tree:bucket_update(bucket()).
 timedout(Address, Bucket) ->
     ?INFO([timing_out, {address, Address}, {bucket, Bucket}]),
-    case get_node(Address, Bucket) of
-	{ok, Node} ->
-	    case Node#node.status of
+    case get_peer(Address, Bucket) of
+	{ok, Peer} ->
+	    case Peer#peer.status of
 		live ->
-		    % mark as stale, return a cache node that might be a suitable replacement
-		    Bucket2 = update_node(Node#node{status=stale}, Bucket),
+		    % mark as stale, return a cache peer that might be a suitable replacement
+		    Bucket2 = update_peer(Peer#peer{status=stale}, Bucket),
 		    pop_cache_new(Bucket2);
 		_ -> 
 		    % if cache or stale already we don't care 
 		    ok(Bucket)
 	    end;
 	none ->
-	    % wtf? we don't even know this node?
+	    % wtf? we don't even know this peer?
 	    % one way this could happen: 
 	    % send N1, sendN1, timedout N1, add N2 (pushing N1 out of stale), timedout N1 
-	    ?WARN([unknown_node_timedout, {address, Address}, {bucket, Bucket}]),
+	    ?WARN([unknown_peer_timedout, {address, Address}, {bucket, Bucket}]),
 	    ok(Bucket)
     end.
 
+-spec dialed(now(), bucket()) -> th_bit_tree:bucket_update(bucket()).
 dialed(Time, Bucket) ->
     ok(Bucket#bucket{last_dialed=Time}).
 
+-spec by_dist('end'(), bucket()) -> list(address()).
 by_dist(End, #bucket{live=Live, stale=Stale}) ->
-    Nodes = pq_maps:to_list(Live) ++ pq_maps:to_list(Stale),
-    % !!! maybe should prefer to return live nodes even if further away
-    Nodes_by_dist = [{th_util:distance(End, Node#node.'end'), Node} || {_Key, Node} <- Nodes],
-    [Node#node.address || {_Dist, Node} <- lists:sort(Nodes_by_dist)].
+    Peers = pq_maps:to_list(Live) ++ pq_maps:to_list(Stale),
+    % !!! maybe should prefer to return live peers even if further away
+    Peers_by_dist = [{th_util:distance(End, Peer#peer.'end'), Peer} || {_Key, Peer} <- Peers],
+    [Peer#peer.address || {_Dist, Peer} <- lists:sort(Peers_by_dist)].
 
+-spec last_touched(bucket()) -> never | now().
 last_touched(#bucket{live=Live, stale=Stale}) ->
     % !!! ugly
     case {pq_maps:peek_hi(Live), pq_maps:peek_hi(Stale)} of
-	{none, none} -> none;
+	{none, none} -> never;
 	{none, {{Last_stale, _}, _}} -> Last_stale;
 	{{{Last_live, _}, _}, none} -> Last_live;
 	{{{Last_live, _}, _}, {{Last_stale, _}, _}} ->
@@ -128,26 +149,30 @@ last_touched(#bucket{live=Live, stale=Stale}) ->
 	    if Last_live > Last_stale -> Last_live; true -> Last_stale end
     end.
 
+-spec last_dialed(bucket()) -> now().
 last_dialed(Bucket) ->
     Bucket#bucket.last_dialed.
 
 % --- functions maintaining the bucket invariants ---
 
 % response format for bit_tree
+-spec ok(bucket()) -> th_bit_tree:bucket_update(bucket()).
 ok(Bucket) ->
     {Lives, Stales, _} = sizes(Bucket),
     {ok, Lives + Stales, Bucket}.
 
 % response format for bit_tree
+-spec split(bucket()) -> th_bit_tree:bucket_update(bucket()). 
 split(Bucket) ->
-    Nodes = to_list(Bucket),
-    NodesF = [Node#node{suffix=Suffix2} || #node{suffix=[false|Suffix2]}=Node <- Nodes],
-    NodesT = [Node#node{suffix=Suffix2} || #node{suffix=[true|Suffix2]}=Node <- Nodes],
-    {split, ok(from_list(NodesF)), ok(from_list(NodesT))}.
+    Peers = to_list(Bucket),
+    PeersF = [Peer#peer{suffix=Suffix2} || #peer{suffix=[false|Suffix2]}=Peer <- Peers],
+    PeersT = [Peer#peer{suffix=Suffix2} || #peer{suffix=[true|Suffix2]}=Peer <- Peers],
+    {split, ok(from_list(PeersF)), ok(from_list(PeersT))}.
 
 % assumes Address is not already in Bucket, otherwise crashes
-new_node(Address, Suffix, Time, Bucket, May_split) ->
-    Node = #node{
+-spec new_peer(address(), bits(), now(), bucket(), boolean()) -> th_bit_tree:bucket_update(bucket()).
+new_peer(Address, Suffix, Time, Bucket, May_split) ->
+    Peer = #peer{
       address = Address,
       'end' = th_util:to_end(Address),
       suffix = Suffix,
@@ -158,85 +183,80 @@ new_node(Address, Suffix, Time, Bucket, May_split) ->
     if
 	Lives + Stales < ?K ->
 	    % space left in live
-	    ?INFO([adding, {node, Node}, {bucket, Bucket}]),
-	    ok(add_node(Node#node{status=live}, Bucket));
+	    ?INFO([adding, {peer, Peer}, {bucket, Bucket}]),
+	    ok(add_peer(Peer#peer{status=live}, Bucket));
 	(Lives < ?K) and (Stales > 0) ->
 	    % space left in live if we push something out of stale
-	    ?INFO([adding, {node, Node}, {bucket, Bucket}]),
+	    ?INFO([adding, {peer, Peer}, {bucket, Bucket}]),
 	    Bucket2 = drop_stale(Bucket),
-	    ok(add_node(Node#node{status=live}, Bucket2));
+	    ok(add_peer(Peer#peer{status=live}, Bucket2));
 	May_split and (Suffix /= []) ->
 	    % allowed to split the bucket to make space
-	    ?INFO([splitting, {node, Node}, {bucket, Bucket}]),
-	    {split, {ok, BucketF}, {ok, BucketT}} = split(Bucket),
+	    ?INFO([splitting, {peer, Peer}, {bucket, Bucket}]),
+	    {split, {ok, _, BucketF}=OkF, {ok, _, BucketT}=OkT} = split(Bucket),
 	    [Bit | Suffix2] = Suffix,
 	    case Bit of
 		false ->
-		    BucketF2 = new_node(Address, Suffix2, Time, BucketF, May_split),
-		    {split, BucketF2, BucketT};
+		    BucketF2 = new_peer(Address, Suffix2, Time, BucketF, May_split),
+		    {split, BucketF2, OkT};
 		true ->
-		    BucketT2 = new_node(Address, Suffix2, Time, BucketT, May_split),
-		    {split, BucketF, BucketT2}
+		    BucketT2 = new_peer(Address, Suffix2, Time, BucketT, May_split),
+		    {split, OkF, BucketT2}
 	    end;
 	true ->
 	    % not allowed to split, will have to go in the cache
-	    ?INFO([caching, {node, Node}, {bucket, Bucket}]),
-	    ok(add_node(Node#node{status=cache}, bucket))
+	    ?INFO([caching, {peer, Peer}, {bucket, Bucket}]),
+	    ok(add_peer(Peer#peer{status=cache}, Bucket))
     end.
 
-% --- internal functions handling the (nodes -> live/stale/cache) mapping ---
+% --- internal functions handling the (peers -> live/stale/cache) mapping ---
 
-empty() ->
-    #bucket{
-       last_dialed = never,
-       nodes = gb_trees:empty(),
-       live = pq_maps:empty(),
-       stale = pq_maps:empty(),
-       cache = pq_maps:empty()
-      }.
-
-% assumes Node does not already exist in Bucket, crashes otherwise
-add_node(#node{address=Address, status=Status, last_seen=Last_seen}=Node, 
-	 #bucket{nodes=Nodes, live=Live, stale=Stale, cache=Cache}=Bucket) ->
-    Nodes2 = gb_trees:insert(Address, {Status, Last_seen}, Nodes),
+% assumes Peer does not already exist in Bucket, crashes otherwise
+-spec add_peer(peer(), bucket()) -> bucket().
+add_peer(#peer{address=Address, status=Status, last_seen=Last_seen}=Peer, 
+	 #bucket{peers=Peers, live=Live, stale=Stale, cache=Cache}=Bucket) ->
+    Peers2 = gb_trees:insert(Address, {Status, Last_seen}, Peers),
     case Status of
 	live ->
-	    Live2 = pq_maps:push_one({Last_seen, Address}, Node, Live),
-	    Bucket#bucket{nodes=Nodes2, live=Live2};
+	    Live2 = pq_maps:push_one({Last_seen, Address}, Peer, Live),
+	    Bucket#bucket{peers=Peers2, live=Live2};
 	stale ->
-	    Stale2 = pq_maps:push_one({Last_seen, Address}, Node, Stale),
-	    Bucket#bucket{nodes=Nodes2, stale=Stale2};
+	    Stale2 = pq_maps:push_one({Last_seen, Address}, Peer, Stale),
+	    Bucket#bucket{peers=Peers2, stale=Stale2};
 	cache ->
-	    Cache2 = pq_maps:push_one({Last_seen, Address}, Node, Cache),
-	    Bucket#bucket{nodes=Nodes2, cache=Cache2}
+	    Cache2 = pq_maps:push_one({Last_seen, Address}, Peer, Cache),
+	    Bucket#bucket{peers=Peers2, cache=Cache2}
     end.
     
-% assumes Node already exists in Bucket, crashes otherwise
-del_node(#node{address=Address}, Bucket) ->
-    del_node(Address, Bucket);
-del_node(Address,
-	 #bucket{nodes=Nodes, live=Live, stale=Stale, cache=Cache}=Bucket) ->
-    {Status, Last_seen} = gb_trees:get(Address, Nodes),
-    Nodes2 = gb_trees:delete(Address, Nodes),
+% assumes Peer already exists in Bucket, crashes otherwise
+-spec del_peer(peer() | address(), bucket()) -> bucket().
+del_peer(#peer{address=Address}, Bucket) ->
+    del_peer(Address, Bucket);
+del_peer(Address,
+	 #bucket{peers=Peers, live=Live, stale=Stale, cache=Cache}=Bucket) ->
+    {Status, Last_seen} = gb_trees:get(Address, Peers),
+    Peers2 = gb_trees:delete(Address, Peers),
     case Status of
 	live ->
 	    Live2 = pq_maps:delete({Last_seen, Address}, Live),
-	    Bucket#bucket{nodes=Nodes2, live=Live2};
+	    Bucket#bucket{peers=Peers2, live=Live2};
 	stale ->
 	    Stale2 = pq_maps:delete({Last_seen, Address}, Stale),
-	    Bucket#bucket{nodes=Nodes2, stale=Stale2};
+	    Bucket#bucket{peers=Peers2, stale=Stale2};
 	cache ->
 	    Cache2 = pq_maps:delete({Last_seen, Address}, Cache),
-	    Bucket#bucket{nodes=Nodes2, cache=Cache2}
+	    Bucket#bucket{peers=Peers2, cache=Cache2}
     end.
 
-% assumes Node already exists in Bucket, crashes otherwise
-update_node(Node, Bucket) ->
-    add_node(Node, del_node(Node, Bucket)).
+% assumes Peer already exists in Bucket, crashes otherwise
+-spec update_peer(peer(), bucket()) -> bucket().
+update_peer(Peer, Bucket) ->
+    add_peer(Peer, del_peer(Peer, Bucket)).
 
-get_node(Address, 
-	 #bucket{nodes=Nodes, live=Live, stale=Stale, cache=Cache}) ->
-    case gb_trees:lookup(Address, Nodes) of
+-spec get_peer(address(), bucket()) -> none | {ok, peer()}.
+get_peer(Address, 
+	 #bucket{peers=Peers, live=Live, stale=Stale, cache=Cache}) ->
+    case gb_trees:lookup(Address, Peers) of
 	{value, {Status, Last_seen}} ->
 	    case Status of 
 		live ->
@@ -250,36 +270,42 @@ get_node(Address,
 	    none
     end.
 
+-spec to_list(bucket()) -> list(peer()).
 to_list(#bucket{live=Live, stale=Stale, cache=Cache}) ->
     pq_maps:values(Live) ++ pq_maps:values(Stale) ++ pq_maps:values(Cache).
 
-from_list(Nodes) ->
-    lists:foldl(fun add_node/2, empty(), Nodes).
+-spec from_list(list(peer())) -> bucket().
+from_list(Peers) ->
+    lists:foldl(fun add_peer/2, empty(), Peers).
 
+-spec sizes(bucket()) -> {integer(), integer(), integer()}.
 sizes(#bucket{live=Live, stale=Stale, cache=Cache}) ->
     {pq_maps:size(Live), pq_maps:size(Stale), pq_maps:size(Cache)}.
 
-% drop the oldest stale node, crashes if none exist
-drop_stale(#bucket{nodes=Nodes, stale=Stale}=Bucket) ->
-    {_Key, #node{address=Address}, Stale2} = pq_maps:pop_hi(Stale),
-    Nodes2 = gb_trees:delete(Address, Nodes),
-    Bucket#bucket{nodes=Nodes2, stale=Stale2}.
+% drop the oldest stale peer, crashes if none exist
+-spec drop_stale(bucket()) -> bucket().
+drop_stale(#bucket{peers=Peers, stale=Stale}=Bucket) ->
+    {_Key, #peer{address=Address}, Stale2} = pq_maps:pop_hi(Stale),
+    Peers2 = gb_trees:delete(Address, Peers),
+    Bucket#bucket{peers=Peers2, stale=Stale2}.
 
-% return most recently seen cache node, if any exist
-pop_cache_new(#bucket{nodes=Nodes, cache=Cache}=Bucket) ->
+% return most recently seen cache peer, if any exist
+-spec pop_cache_new(bucket()) -> th_bit_tree:bucket_update(bucket()) | {ping, address(), th_bit_tree:bucket_update(bucket())}.
+pop_cache_new(#bucket{peers=Peers, cache=Cache}=Bucket) ->
     case pq_maps:pop_hi(Cache) of
-	{_Key, #node{address=Address}=Node, Cache2} ->
-	    Nodes2 = gb_trees:delete(Address, Nodes),
-	    {ping, Node#node.address, ok(Bucket#bucket{nodes=Nodes2, cache=Cache2})};
+	{_Key, #peer{address=Address}=Peer, Cache2} ->
+	    Peers2 = gb_trees:delete(Address, Peers),
+	    {ping, Peer#peer.address, ok(Bucket#bucket{peers=Peers2, cache=Cache2})};
 	false ->
 	    ok(Bucket)
     end.
 
-% return the oldest live node
+% return the oldest live peer
+-spec peek_live_old(bucket()) -> none | {ok, peer()}.
 peek_live_old(#bucket{live=Live}) ->
     case pq_maps:peek_lo(Live) of
 	none -> none;
-	{_, Node} -> {ok, Node}
+	{_, Peer} -> {ok, Peer}
     end.
 	     	    
 % --- end ---
