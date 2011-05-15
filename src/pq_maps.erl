cf0b9b8f272f877b7f1a12a6e94b5f4ec5cc3504
Jamie Brandon <jamie@scattered-thoughts.net>
2011-05-15 20:07:23 +0800

    Types for pq_sets

--- src/pq_maps.erl
+++ src/pq_maps.erl
@@ -5,23 +5,23 @@
 
 -export([empty/0, is_empty/1, size/1, push_one/3, pop_lo/1, pop_hi/1, push/2, pop_los/2, peek_lo/1, peek_hi/1, to_list/1, from_list/1, get/2, delete/2, values/1]).
 
--type pq_map() :: gb_tree().
+-type pq() :: gb_tree().
 
 -export_types([pq_map/0]).
 
--spec empty() -> pq_map().
+-spec empty() -> pq().
 empty() ->
     gb_trees:empty().
 
--spec is_empty(pq_map()) -> boolean().
+-spec is_empty(pq()) -> boolean().
 is_empty(Q) ->
     gb_trees:is_empty(Q).
 
--spec size(pq_map()) -> integer().
+-spec size(pq()) -> integer().
 size(Q) ->
     gb_trees:size(Q).
 
--spec push_one(term(), term(), pq_map()) -> pq_map().
+-spec push_one(term(), term(), pq()) -> pq().
 push_one(Key, Value, Q) ->
     case gb_trees:lookup(Key, Q)  of
 	{value, _} ->
@@ -30,7 +30,7 @@ push_one(Key, Value, Q) ->
 	    gb_trees:insert(Key, Value, Q)
     end.
 
--spec pop_lo(pq_map()) -> none | {Key::term(), Value::term(), pq_map()}.
+-spec pop_lo(pq()) -> none | {Key::term(), Value::term(), pq()}.
 pop_lo(Q) ->
     case is_empty(Q) of 
 	true -> none;
@@ -38,7 +38,7 @@ pop_lo(Q) ->
 	    gb_trees:take_smallest(Q)
     end.
 
--spec pop_hi(pq_map()) -> none | {Key::term(), Value::term(), pq_map()}.
+-spec pop_hi(pq()) -> none | {Key::term(), Value::term(), pq()}.
 pop_hi(Q) ->
     case is_empty(Q) of 
 	true -> none;
@@ -46,11 +46,11 @@ pop_hi(Q) ->
 	    gb_trees:take_largest(Q)
     end.
 
--spec push(list({term(), term()}), pq_map()) -> pq_map().
+-spec push(list({term(), term()}), pq()) -> pq().
 push(Items, Q) ->
     lists:foldl(fun ({Key, Value}, Q2) -> push_one(Key, Value, Q2) end, Q, Items).
 
--spec pop_los(pq_map(), integer()) -> {list({Key::term(), Value::term()}), pq_map()}.
+-spec pop_los(pq(), integer()) -> {list({Key::term(), Value::term()}), pq()}.
 pop_los(Q, 0) ->
     {[], Q};
 pop_los(Q, K) when K > 0 ->
@@ -62,14 +62,14 @@ pop_los(Q, K) when K > 0 ->
 	    {[{Key, Value} | Items], Q3}
     end.
 
--spec peek_lo(pq_map()) -> none | {Key::term(), Value::term()}.
+-spec peek_lo(pq()) -> none | {Key::term(), Value::term()}.
 peek_lo(Q) ->
     case is_empty(Q) of
 	true -> none;
 	false -> gb_trees:smallest(Q)
     end.
 
--spec peek_hi(pq_map()) -> none | {Key::term(), Value::term()}.
+-spec peek_hi(pq()) -> none | {Key::term(), Value::term()}.
 peek_hi(Q) ->
     case is_empty(Q) of
 	true -> none;
@@ -77,22 +77,22 @@ peek_hi(Q) ->
     end.
 
 % assumes Key is in Q, crashes otherwise 
--spec get(term(), pq_map()) -> Value::term(). 
+-spec get(term(), pq()) -> Value::term(). 
 get(Key, Q) ->
     gb_trees:get(Key, Q).
 
--spec delete(term(), pq_map()) -> pq_map().
+-spec delete(term(), pq()) -> pq().
 delete(Key, Q) ->
     gb_trees:delete_any(Key, Q).
 
--spec to_list(pq_map()) -> list({Key::term(), Value::term()}).
+-spec to_list(pq()) -> list({Key::term(), Value::term()}).
 to_list(Q) ->
     gb_trees:to_list(Q).
 
--spec from_list(list({Key::term(), Value::term()})) -> pq_map().
+-spec from_list(list({Key::term(), Value::term()})) -> pq().
 from_list(List) ->
     push(List, empty()).
 
--spec values(pq_map()) -> list(Value::term()).
+-spec values(pq()) -> list(Value::term()).
 values(Q) ->
     gb_trees:values(Q).
--- src/pq_sets.erl
+++ src/pq_sets.erl
@@ -5,49 +5,61 @@
 
 -export([empty/0, is_empty/1, size/1, push_one/2, pop_one/1, push/2, pop/2, peek/1, to_list/1, from_list/1, delete/2]).
 
+-type pq() :: gb_set().
+
+-spec empty() -> pq().
 empty() ->
     gb_sets:empty().
 
+-spec is_empty(pq()) -> boolean().
 is_empty(Q) ->
     gb_sets:is_empty(Q).
 
+-spec size(pq()) -> integer().
 size(Q) ->
     gb_sets:size(Q).
 
+-spec push_one({term(), term()}, pq()) -> pq().
 push_one({Prio, Item}, Q) ->
     gb_sets:add_element({Prio, Item}, Q).
 
+-spec pop_one(pq()) -> none | {{Prio::term(), Item::term()}, pq()}.
 pop_one(Q) ->
     case is_empty(Q) of 
-	true -> false;
+	true -> none;
 	false ->
 	    {{Prio, Item}, Q2} = gb_sets:take_smallest(Q),
 	    {{Prio, Item}, Q2}
     end.
 
+-spec push(list({Prio::term(), Item::term()}), pq()) -> pq().
 push(Items, Q) ->
     lists:foldl(fun ({Prio, Item}, Q2) -> push_one({Prio, Item}, Q2) end, Q, Items).
 
+-spec pop(pq(), integer()) -> {list({Prio::term(), Value::term()}), pq()}.
 pop(Q, 0) ->
     {[], Q};
 pop(Q, K) when K > 0 ->
-    case is_empty(Q) of
-	true -> 
+    case pop_one(Q) of
+	none -> 
 	    {[], Q};
-	false ->
-	    {Item, Q2} = pop_one(Q),
+	{Item, Q2} ->
 	    {Items, Q3} = pop(Q2, K-1),
 	    {[Item | Items], Q3}
     end.
 
+-spec peek(pq()) -> {Prio::term(), Value::term()}.
 peek(Q) ->
     gb_sets:smallest(Q).
 
+-spec delete({Prio::term(), Value::term()}, pq()) -> pq().
 delete({Prio, Item}, Q) ->
     gb_sets:delete_any({Prio, Item}, Q).
 
+-spec to_list(pq()) -> list({Prio::term(), Value::term()}).
 to_list(Q) ->
     gb_sets:to_list(Q).
 
+-spec from_list(list({Prio::term(), Value::term()})) -> pq().
 from_list(List) ->
     gb_sets:from_list(List).
