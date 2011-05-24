53d0cf190de25934bdd299311d35bf3cc2653359
Jamie Brandon <jamie@scattered-thoughts.net>
2011-05-24 18:06:12 +0800

    Types and bugfixes for th_telex

--- src/th_telex.erl
+++ src/th_telex.erl
@@ -3,11 +3,27 @@
 -module(th_telex).
 
 -include("conf.hrl").
+-include("types.hrl").
 
 -export([encode/1, decode/1, get/2, set/3, update/4, end_signal/1, see_command/1, key_type/1]).
 
-% --- api ---
+-type json_string() :: atom | binary().
+-type json_number() :: integer() | float().
+-type json_array() :: [json()].
+-type json_object() :: {struct, [{json_string(), json()}]}.
+-type json() :: json_string() | json_number() | 
+		json_array() | json_object().
+-type telex() :: json().
 
+-type key_type() :: header | signal | command | normal.
+
+-type path() :: json_string() | integer() | list(path()).
+
+-export_type([json_string/0, json_number/0, json_array/0, json_object/0, json/0, telex/0, key_type/0, path/0]).
+
+% --- encoding / decoding ---
+
+-spec encode(telex()) -> binary().
 encode(Telex) ->
     Json =
 	try
@@ -21,6 +37,7 @@ encode(Telex) ->
 	false -> erlang:error({telex, encode, too_big, Telex})
     end.
 
+-spec decode(binary()) -> telex().
 decode(Json) ->
     case byte_size(Json) =< ?TELEX_MAX_BYTES of
 	true -> ok;
@@ -33,67 +50,80 @@ decode(Json) ->
 	    erlang:error({telex, decode, Error, Json})
     end.
 
-get(Telex, Key) when is_list(Key) ->
-    get(Telex, list_to_binary(Key));
-get(Telex, Key) when is_atom(Key) ->
-    get(Telex, list_to_binary(atom_to_list(Key)));
-get({struct, Telex}, Key) when is_list(Telex) and is_binary(Key) ->
-    case lists:keyfind(Key, 1, Telex) of
+% --- json accessors ---
+
+-spec get(json(), path()) -> {ok, json()} | {error, not_found}.
+get(Json, Path) when is_atom(Path) ->
+    get(Json, list_to_binary(atom_to_list(Path)));
+get({struct, Json}, Path) when is_list(Json) and is_binary(Path) ->
+    case lists:keyfind(Path, 1, Json) of
 	false -> {error, not_found};
-	{Key, Value} -> {ok, Value}
+	{Path, Value} -> {ok, Value}
     end;
-get(Telex, Index) when is_list(Telex) and is_integer(Index) ->
-    case Index =< length(Telex) of
+get(Json, Index) when is_list(Json) and is_integer(Index) ->
+    case Index =< length(Json) of
 	false -> {error, not_found};
-	true -> {ok, lists:nth(Index, Telex)}
+	true -> {ok, lists:nth(Index, Json)}
     end;
-get(Telex, Path) when is_tuple(Path) ->
-    get_path(Telex, tuple_to_list(Path));
-get(_Telex, _Key) ->
+get(Json, Path) when is_list(Path) ->
+    get_list(Json, Path);
+get(_Json, _Path) ->
     % !!! not too happy about obscuring badarg
     {error, not_found}.
 
-get_path(Telex, []) ->
-    Telex;
-get_path(Telex, [Path_elem | Path]) ->
-    case get(Telex, Path_elem) of
+-spec get_list(json(), list(path())) -> {ok, json()} | {error, not_found}.
+get_list(Json, []) ->
+    {ok, Json};
+get_list(Json, [Tuple_elem | Tuple]) ->
+    case get(Json, Tuple_elem) of
 	{error, not_found} -> {error, not_found};
-	{ok, Telex2} -> get_path(Telex2, Path)
+	{ok, Json2} -> get_list(Json2, Tuple)
     end.
 
-set(Telex, Key, Value) ->
-    update(Telex, Key, fun (_T) -> Value end, Value).
+-spec set(json(), path(), json()) -> json().
+set(Json, Path, Value) ->
+    update(Json, Path, fun (_T) -> Value end, Value).
 
-% !!! check new value is valid json
-update(Telex, Key, F, Default) when is_list(Key) ->
-    update(Telex, list_to_binary(Key), F, Default);
-update(Telex, Key, F, Default) when is_atom(Key) ->
-    update(Telex, list_to_binary(atom_to_list(Key)), F, Default);
-update({struct, Telex}, Key, F, Default) when is_list(Telex) and is_binary(Key) ->
-    case lists:keyfind(Key, 1, Telex) of
-	{Key, Value} -> {struct, lists:keyreplace(Key, 1, Telex, {Key, F(Value)})};
-	false -> {struct, [{Key, Default} | Telex]}
+-spec update(json(), path(), fun((json()) -> json()), json()) -> json().
+update(Json, Path, F, Default) when is_atom(Path) ->
+    update(Json, list_to_binary(atom_to_list(Path)), F, Default);
+update({struct, Json}, Path, F, Default) when is_list(Json) and is_binary(Path) ->
+    case lists:keyfind(Path, 1, Json) of
+	{Path, Value} -> {struct, lists:keyreplace(Path, 1, Json, {Path, F(Value)})};
+	false -> {struct, [{Path, Default} | Json]}
     end;
-update(Telex, Index, F, _Default) when is_list(Telex) and is_integer(Index) ->
+update(Json, Index, F, _Default) when is_list(Json) and is_integer(Index) ->
     % !!! behaviour for out of range indexes?
-    th_util:set_nth(Index, Telex, F(lists:nth(Index, Telex)));
-update(Telex, Path, F, Default) when is_tuple(Path) ->
-    update_path(Telex, tuple_to_list(Path), F, Default).
+    set_nth(Index, Json, F(lists:nth(Index, Json)));
+update(Json, Path, F, Default) when is_list(Path) ->
+    update_list(Json, Path, F, Default).
 
-update_path(Telex, [], F, _Default) ->
-    F(Telex);
-update_path(Telex, [Path_elem | Path], F, Default) ->
-    update(Telex, Path_elem, fun (T) -> update_path(T, Path, F, Default) end, Default).
+-spec update_list(json(), list(path()), fun((json()) -> json()), json()) -> json().
+update_list(Json, [], F, _Default) ->
+    F(Json);
+update_list(Json, [Tuple_elem | Tuple], F, Default) ->
+    update(Json, Tuple_elem, fun (T) -> update_list(T, Tuple, F, Default) end, Default).
 
+-spec set_nth(integer(), list(), term()) -> list().
+set_nth(1, [_Head | Tail], Value) ->
+    [Value | Tail];
+set_nth(N, [Head | Tail], Value) when N>1 ->
+    [Head | set_nth(N-1, Tail, Value)].
+
+% --- common telexes ---
+
+-spec end_signal('end'()) -> json_object().	
 end_signal({'end', _}=End) ->
     {struct, [{'+end', th_util:end_to_hex(End)}]}.
 
+-spec see_command(list(address())) -> json_object().
 see_command(Addresses) ->
     {struct, [{'.see', [th_util:address_to_binary(Address) || Address <- Addresses]}]}.
 
 %tap_command(Tap) ->
 %    {struct, [{'.tap', tap_to_json(Tap)}]}.
 
+-spec key_type(binary()) -> key_type().
 key_type(Key) when is_binary(Key) ->
     case Key of
 	<< "_", _/binary >> ->
