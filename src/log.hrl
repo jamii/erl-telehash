e0b99cdddc85dbfd769490fb5f5e331fffe84f36
Jamie Brandon <jamie@scattered-thoughts.net>
2011-10-30 22:54:23 +0000

    Remove useless th_log

--- src/log.hrl
+++ src/log.hrl
@@ -1,4 +1,4 @@
 -define(LOG_PREFIX, [{module, ?MODULE}, {line, ?LINE}, {pid, self()}]).
--define(INFO(Args), th_log:info(?LOG_PREFIX ++ Args)).
--define(WARN(Args), th_log:warn(?LOG_PREFIX ++ Args)).
--define(ERROR(Args), th_log:error(?LOG_PREFIX ++ Args)).
+-define(INFO(Args), error_logger:info_report(?LOG_PREFIX ++ Args)).
+-define(WARN(Args), error_logger:warning_report(?LOG_PREFIX ++ Args)).
+-define(ERROR(Args), error_logger:error_report(?LOG_PREFIX ++ Args)).
